368d95e6ceecf3b28d8229aa675cf392efc4343b
selinyerli <141401610+selinyerli@users.noreply.github.com>
2024-07-12 16:32:05 +0300

    Coins
    Now we can collect coins also it has an audio.

---
+++ Assets/Scripts/Collectables/RotateObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public int rotateSpeed = 1;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+}
