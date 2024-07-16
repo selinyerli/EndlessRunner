7d70db9bc77eb012cf79ce0ae62fd9735f552c8a
selinyerli <141401610+selinyerli@users.noreply.github.com>
2024-07-16 15:26:14 +0300

    Gamestart
    321 and fadein effect is aded.

---
+++ Assets/Scripts/Environment/LevelStarter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class LevelStarter : MonoBehaviour
+{
+    public GameObject countDown3;
+    public GameObject countDown2; 
+    public GameObject countDown1;
+    public GameObject countDownGo;
+    public GameObject fadeIn;
+
+
+    public void Start()
+    {
+        StartCoroutine(CountSequence());
+    }
+    IEnumerator CountSequence()
+    {
+        yield return new WaitForSeconds(1.5f);
+        countDown3.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDown2.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDown1.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDownGo.SetActive(true);
+    }
+}
