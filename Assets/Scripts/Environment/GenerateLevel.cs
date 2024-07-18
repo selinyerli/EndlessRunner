ea7fbd6f87426b6a3e6c6da1bf78fee032be2ef5
selinyerli <141401610+selinyerli@users.noreply.github.com>
2024-07-18 09:41:42 +0300

    clone
    clones are fixed

--- Assets/Scripts/Environment/GenerateLevel.cs
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -1,28 +1,40 @@
+using NUnit.Framework;
 using System.Collections;
 using UnityEngine;
 
 public class GenerateLevel : MonoBehaviour
 {
-    public GameObject[] section;
-    public int zPos = 50;
-    public bool creatingSection = false;
-    public int secNum;
+   
+public GameObject[] section; // Bölüm prefablarý
+public Transform player; // Player objesinin transformu
+public float zPos = 1500; // Yeni bölümün z pozisyonu
+public float sectionLength = 50; // Her bölümün uzunluðu
+public float distanceToGenerate = 50; // Yeni bölüm oluþturmak için gereken mesafe
+private bool creatingSection = false;
 
-    private void Update()
-    {
-        if(creatingSection == false)
-        {
-            creatingSection = true;
-            StartCoroutine(GenerateSection());
-        }
-    }
+    int xd = 0;
 
-    IEnumerator GenerateSection()
+
+private void Update()
+{
+    
+    if (player.position.z >= zPos - distanceToGenerate && !creatingSection)
     {
-        secNum = Random.Range(0, 3);
-        Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
-        zPos += 50;
-        yield return new WaitForSeconds(2);
-        creatingSection = false;
+            Debug.Log(xd);
+            xd++;
+        creatingSection = true;
+        StartCoroutine(GenerateSection());
     }
+}
+
+IEnumerator GenerateSection()
+{
+    int secNum = Random.Range(0, section.Length);
+    GameObject sectionObject = Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+
+
+    zPos += sectionLength;
+    yield return new WaitForSeconds(0.1f); // Çok kýsa bir bekleme süresi
+    creatingSection = false;
+}
 }
